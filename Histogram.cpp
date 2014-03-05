1d15ef99c9778d87d215b6173978358cb5ac1e2f
Orkun Şensebat <ihate88@gmail.com>
2014-03-05 15:28:45 +0100

    Delegated constructors which made sense because of implemented variable binning.
    Requires c++11

--- Histogram.cpp
+++ Histogram.cpp
@@ -1,14 +1,30 @@
 #include "Histogram.h"
 
-Histogram::Histogram(bool (*check)(const vector<Float_t>&), Float_t (*transform)(const vector<Float_t>&), Int_t nbins) {
-   thistogram = new TH1F("name","title", nbins, 0, 2);
+Histogram::Histogram(bool (*check)(const vector<Float_t>&), Float_t (*transform)(const vector<Float_t>&))
+{ 
    this->check = check;
    this->transform = transform;
-   this->nbins = nbins;
-
+   this->min = 0.0;
+   this->max = 0.0;
    mean = 0.0;
    rms = 0.0;
    nentries = 0;
+}
+
+Histogram::Histogram(bool (*check)(const vector<Float_t>&), Float_t (*transform)(const vector<Float_t>&), const std::string& name, Int_t nbins, Float_t min, Float_t max, std::string title) : Histogram(check, transform)
+{
+   if (title == "")
+      title = name;
+   thistogram = new TH1F(name.c_str(), title.c_str(), nbins, min, max);
+   this->nbins = nbins;
+}
+
+Histogram::Histogram(bool (*check)(const vector<Float_t>&), Float_t (*transform)(const vector<Float_t>&), const std::string& name, Int_t nbins, Float_t* edges, std::string title) : Histogram(check, transform) 
+{
+   if (title == "")
+      title = name;
+   thistogram = new TH1F(name.c_str(), title.c_str(), nbins, edges);
+   this->nbins = nbins;
 }
 
 TH1F *Histogram::getHistogram() const {
@@ -21,4 +37,12 @@ Float_t Histogram::getMean() const {
 
 Float_t Histogram::getRMS() const {
    return rms;
+}
+
+Float_t Histogram::getMin() const {
+   return min;
+}
+
+Float_t Histogram::getMax() const {
+   return max;
 }
--- Histogram.h
+++ Histogram.h
@@ -1,5 +1,6 @@
 #include <vector>
 using std::vector;
+#include <string>
 
 #include <TH1F.h>
 
@@ -12,12 +13,18 @@ class Histogram {
 
       Float_t mean;
       Float_t rms;
+      Float_t min;
+      Float_t max;
       Int_t nentries;
 
    public:
-      Histogram(bool (*check)(const vector<Float_t>&), Float_t (*transform)(const vector<Float_t>&), Int_t nbins);
+      Histogram(bool (*check)(const vector<Float_t>&), Float_t (*transform)(const vector<Float_t>&)); 
+      Histogram(bool (*check)(const vector<Float_t>&), Float_t (*transform)(const vector<Float_t>&), const std::string& name, Int_t nbins, Float_t min, Float_t max, std::string title = std::string());
+      Histogram(bool (*check)(const vector<Float_t>&), Float_t (*transform)(const vector<Float_t>&), const std::string& name, Int_t nbins, Float_t* edges, std::string title = std::string()); 
 
       TH1F *getHistogram() const; 
       Float_t getMean() const; 
       Float_t getRMS() const; 
+      Float_t getMin() const; 
+      Float_t getMax() const; 
 };
